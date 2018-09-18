e91e2c7647d63ebed6aca01351752f662d477c0e
Thomas Tsapralis <thomastsapralis@yahoo.gr>
2018-09-18 15:38:36 +0300

    Print tildes from getting window size

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -5,24 +5,33 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <errno.h>
+#include <sys/ioctl.h>
 
 /******************************************* defines *******************************************/
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /******************************************* data *******************************************/
-struct termios orig_termios;
+struct editorConfig
+{
+	struct termios orig_termios;
+	int screenrows;
+	int screencols;
+};
 
-
+struct editorConfig E;
 /******************************************* terminal *******************************************/
 void die(const char *s)
 {
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
 
 void disableRawMode()
 {
-	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+	if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
 	{
 		die("tcsetattr");
 	}
@@ -32,13 +41,13 @@ void disableRawMode()
 void enableRawMode()
 {
 	// read terminal's attibutes into a struck
-	if(tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+	if(tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
 	{
 		die("tcgetattr");
 	}
 	// run this function at exit
 	atexit(disableRawMode);
-	struct termios raw = orig_termios;
+	struct termios raw = E.orig_termios;
 	// turn off the ECHO flag
 	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST); 
@@ -57,6 +66,7 @@ char editorReadKey()
 {
 	int nread;
 	char c;
+	// Wait until i read a character 
 	while((nread = read(STDIN_FILENO, &c, 1)) != 1)
 	{
 		if(nread == -1 && errno != EAGAIN)
@@ -64,15 +74,90 @@ char editorReadKey()
 			die("read");
 		}
 	}
+	
 	return c;
 }
 
+int getCursorPosition(int *rows, int *cols)
+{
+	char buf[32];
+	unsigned int i = 0;
+	// Cursor Position Report is printed in stdin
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+	{
+		return -1;
+	}
+	// Read until R and store the CPR into a buffer
+	while (i < sizeof(buf) - 1)
+	{
+		if(read(STDIN_FILENO, &buf[i], 1) != 1)
+		{
+			break;
+		}
+		if(buf[i] == 'R')
+		{
+			break;
+		}
+		i++;
+	}
+
+	buf[i] = '\0';
+
+	if(buf[0] != '\x1b' || buf[1] != '[')
+	{
+		return -1;
+	}
+	if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+	{
+		return -1;
+	}
+
+	return 0;
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+	struct winsize ws;
+	// Use ioctl or getCursorPosition
+	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+	{
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+		{
+			return -1;
+		}
+		return getCursorPosition(rows, cols);
+	}
+	else
+	{
+		*cols = ws.ws_col;
+		*rows = ws.ws_row;
+		return 0;
+	}
+}
+
 /******************************************* output *******************************************/
+
+void editorDrawRows()
+{
+	int y;
+	for(y = 0; y < E.screenrows; y++)
+	{
+		write(STDOUT_FILENO, "~", 1);
+
+		if(y < E.screenrows - 1)
+		{
+			write(STDOUT_FILENO, "\r\n", 2);
+		}
+	}
+}
 
 void editorRefreshScreen()
 {
 	write(STDOUT_FILENO, "\x1b[2J", 4);
 	write(STDOUT_FILENO, "\x1b[H",3);
+
+	editorDrawRows();
+	write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 /******************************************* input *******************************************/
@@ -85,6 +170,8 @@ void editorProcessKeypress()
 	{
 		case CTRL_KEY('q'):
 		{
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
 			exit(0);
 		} break;
 	}
@@ -92,16 +179,29 @@ void editorProcessKeypress()
 
 /******************************************* init *******************************************/
 
+void initEditor()
+{
+	if(getWindowSize(&E.screenrows, &E.screencols) == -1)
+	{
+		die("getWindowSize");
+	}
+}
+
 int main(int argc, char **argv)
 {
-
+	// Change some terminal attributes
 	enableRawMode();
+	// Determines the screen size
+	initEditor();
 
 	while(1)
 	{
+		// Clears the screen / draws tildes 
 		editorRefreshScreen();
+		// Waits until reads a key
 		editorProcessKeypress();
+		
 	}
-	
+
 	return 0;
 }
