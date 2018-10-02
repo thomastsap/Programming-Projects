e0418a080b414d442e17be1ab201780942e1d2dc
Thomas Tsapralis <thomastsapralis@yahoo.gr>
2018-10-02 10:02:08 +0300

    Write Buffer

--- Text Editor/kilo.c
+++ Text Editor/kilo.c
@@ -6,6 +6,7 @@
 #include <ctype.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <string.h>
 
 /******************************************* defines *******************************************/
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -135,29 +136,67 @@ int getWindowSize(int *rows, int *cols)
 	}
 }
 
+/*** append buffer ***/
+struct abuf
+{
+	char *b;
+	int len;
+};
+
+#define ABUF_INIT {NULL, 0}
+
+void abAppend(struct abuf *ab, const char *s, int len)
+{
+	char *new = realloc(ab->b, ab->len + len);
+
+	if (new == NULL) return;
+	memcpy(&new[ab->len], s, len);
+	ab->b = new;
+	ab->len += len;
+
+}
+
+void abFree(struct abuf *ab)
+{
+	// deallocates the dynamic memory used by abuf
+	free(ab->b);
+}
+
 /******************************************* output *******************************************/
 
-void editorDrawRows()
+void editorDrawRows(struct abuf *ab)
 {
 	int y;
 	for(y = 0; y < E.screenrows; y++)
 	{
-		write(STDOUT_FILENO, "~", 1);
+		abAppend(ab, "~", 1);
+		//write(STDOUT_FILENO, "~", 1);
 
+		abAppend(ab, "\x1b[K", 3);
 		if(y < E.screenrows - 1)
 		{
-			write(STDOUT_FILENO, "\r\n", 2);
+			abAppend(ab, "\r\n", 2);
+			//write(STDOUT_FILENO, "\r\n", 2);
 		}
 	}
 }
 
 void editorRefreshScreen()
 {
-	write(STDOUT_FILENO, "\x1b[2J", 4);
-	write(STDOUT_FILENO, "\x1b[H",3);
+	struct abuf ab = ABUF_INIT; // equals to struct abuf ab = {NULL	, 0};
 
-	editorDrawRows();
-	write(STDOUT_FILENO, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25l", 6); // hide the cursor
+	//abAppend(&ab, "\x1b[2J, 4");
+	abAppend(&ab, "\x1b[H", 3);
+	//write(STDOUT_FILENO, "\x1b[2J", 4);
+	//write(STDOUT_FILENO, "\x1b[H",3);
+
+	editorDrawRows(&ab); // call by reference
+	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, "\x1b[?25h", 6); // hide the cursor
+
+	write(STDOUT_FILENO, ab.b, ab.len); 
+	abFree(&ab);
 }
 
 /******************************************* input *******************************************/
